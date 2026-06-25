d820cceae29b34e7479d31ba23286d8c3280a433
abdulbasset-benz <benz.abdelbasset@gmail.com>
2026-06-25 16:01:09 +0100

    implement data collection hooks

---
+++ apps/web/src/features/auth/hooks/useAuth.ts
@@ -0,0 +1,25 @@
+import { useCurrentUser } from "./useCurrentUser";
+
+export function useAuth() {
+  const query = useCurrentUser();
+
+  return {
+    ...query,
+
+    isAuthenticated: !!query.data?.user,
+
+    isOnboarded: !!query.data?.organizationProfile?.onboardingCompletedAt,
+
+    user: query.data?.user,
+
+    session: query.data?.session,
+
+    organization: query.data?.organization,
+
+    member: query.data?.member,
+
+    organizationProfile: query.data?.organizationProfile,
+
+    subscription: query.data?.subscription,
+  };
+}
---
+++ apps/web/src/features/auth/hooks/useCurrentUser.ts
@@ -0,0 +1,13 @@
+import { useQuery } from "@tanstack/react-query";
+import { getMe } from "../api/get-me";
+
+export function useCurrentUser() {
+  return useQuery({
+    queryKey: ["me"],
+    queryFn: getMe,
+
+    retry: false,
+
+    staleTime: 1000 * 60 * 5,
+  });
+}
