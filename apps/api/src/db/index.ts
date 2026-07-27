f73d39e247c2fc3c97f3e29b4477c919faa2243e
abdulbasset-benz <benz.abdelbasset@gmail.com>
2026-07-27 11:36:44 +0100

    minor changes

--- apps/api/src/db/index.ts
+++ apps/api/src/db/index.ts
@@ -1,12 +1,12 @@
-import { drizzle } from 'drizzle-orm/postgres-js'
-import postgres from 'postgres'
-import * as schema from './schema'
-import 'dotenv/config'
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import * as schema from "@freelo/shared/db/schema/index.js";
+import "dotenv/config";
 
 const client = postgres(process.env.DATABASE_URL!, {
   max: 10,
-  ssl: 'require',
-})
+  ssl: "require",
+});
 
-export const db = drizzle(client, { schema })
-export type DB = typeof db
+export const db = drizzle(client, { schema });
+export type DB = typeof db;
